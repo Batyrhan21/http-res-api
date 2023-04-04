dc06d9a12accc1cb312d411a9bd564a9e88d7790
Batyrhan21 <baatyr3030@gmail.com>
2023-04-04 12:51:19 +0600

    basic settings have been configurated

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,18 @@
+package apiserver
+
+
+type APIServer struct{
+	config *Config
+}
+
+
+func  New(config * Config ) *APIServer {
+	return &APIServer{
+		config: config,
+	}
+}
+
+
+func (s *APIServer) Start() error {
+	return nil
+}
